5f973ce6de5c78c8b01b93d1522bb51c788a8536
utilizatorvalid <vasilegroza3@gmail.com>
2017-08-05 14:50:57 +0300

    about page created

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  author:String = "Groza Vasile"
   constructor() { }
 
   ngOnInit() {
+    $('.collapsible').collapsible();
   }
 
 }
